e6f09f8e98f9d79763b15950e60e6ff2544011c1
holdenhao <holdenhao@gmail.com>
2017-10-28 16:19:52 +0800

    comment out requirement for paster

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(name='wcc.videolink',
       # The next two lines may be deleted after you no longer need
       # addcontent support from paster and before you distribute
       # your package.
-      setup_requires=["PasteScript"],
-      paster_plugins=["templer.localcommands"],
+      #setup_requires=["PasteScript"],
+      #paster_plugins=["templer.localcommands"],
 
       )
